e6b773d001512f181f400288c2e183b797d5ccda
alexis barrientos <alexis.bj16@gmail.com>
2023-04-19 23:04:04 -0300

    segunda pre-entrega alexis barrientos

---
+++ README.md
@@ -0,0 +1,17 @@
+Este código es un pequeño prototipo de billetera virtual que permite a los usuarios iniciar sesión, ver su saldo, depositar dinero y transferir dinero a otros usuarios.
+
+El código comienza con una lista de usuarios:
+usuario n°1 Alexis ,contraseña : 4432. 
+usuario n°2 Julia , contraseña : 1234.
+usuario n °3 Juan , contraseña : 5678.
+que cada usuario tiene su saldo .
+tiene dos funciones una para buscar al usuario y devolver sus datos y otra funcion que es para el ingreso que se pide que ingrese usuario y contraseña (login).
+
+Una vez que el usuario entra tiene un menu (ver saldo , trasferir y ingresar dinero).
+
+se utiliza prompt y alert para interactuar con el usuario.
+
+
+y del lado del html se hizo la pag de ingreso y la de registrarse y un avance de la pag una vez que entra el usuario
+
+
--- js/index.js
+++ js/index.js
@@ -1,63 +1,84 @@
+let users = [  
+    {    name: "alexis",    password: "4432",    cash: 35000  }, 
+    {    name: "julia",    password: "1234",    cash: 10000  }, 
+    {    name: "juan",    password: "5678",    cash: 50000  }];
 
-// // variables
-// let usuario = "alexis";
-// let contra ="4432";
-// let exito = login();
+function buscarUsuario(name) {
+  return users.find(user => user.name === name);
+}
 
-// // ingreso
-// function login(){
-//     let ingreso = false;
+function login() {
+  let intentos = 3;
 
-//     for (let i =2; i >= 0 ;i--){
-//         let user = prompt("ingresar usuario");
-//         let pin = prompt ("ingrese contraseña");
+  while (intentos > 0) {
+    let name = prompt("Ingrese su usuario:");
+    let password = prompt("Ingrese su contraseña:");
 
-//         if (user==usuario && pin == contra) {
-//             alert ("bienvenido " + usuario);
-//             ingreso = true;
-//             break;
-//         } else {
-//             alert("Error de Usuario o Contraseña te quedan " + i + " oportunidades");
-//         }
-//     }
-//     return ingreso;
-// }
+    let user = buscarUsuario(name);
 
-// if (exito){
-//     let saldo = 35000;
-//     let menu = prompt (usuario + "Elija una opción: \n1- ver saldo. \n2- ingresar dinero. \n3- transferir dinero. \n5 Presionar 0 para finalizar.");
+    if (user && user.password === password) {
+      alert("¡Bienvenido, " + name + "!");
+      return user;
+    } else {
+      intentos--;
+      alert("Nombre de usuario o contraseña incorrecta. Le quedan " + intentos + " intentos.");
+    }
+  }
 
-//     while(menu != "0"){
-//         switch (menu) {
-//             case "1":
-//                 alert("su saldo es $" + saldo);
-//             break;
-//             case "2":
-//                 let depositar = parseInt(prompt("Ingresar monto"));
-//                 saldo =saldo + depositar;
-//                 alert("Su saldo es de " + saldo);
-//             break;
-//             case "3":
-//                 let transferir= parseInt(prompt("Ingresar monto a transferir "));
-//                 if (transferir <= saldo) {
-//                     saldo = saldo - transferir;
-//                     alert("Transferiste $ " + transferir + ", tu saldo actual es $ " + saldo);
-//                     if(transferir<= 50000){
-//                         saldo = saldo - transferir;
-//                         alert("Transferiste $ " + transferir + ", tu saldo actual es $ " + saldo);
-//                     } else {
-//                         alert("Excede el limite diario de transferencia");
-//                     }
-//                 } else {
-//                     alert("Saldo insuficiente, Ingrese otro monto");
-//                 }
-//             break;
-//             default:
-//                 alert("Opcion no valida");
-//             break;
-//         }
-//         menu = prompt (usuario + "Elija una opción: \n1- ver saldo. \n2- ingresar dinero. \n3- transferir dinero. \n5 Presionar 0 para finalizar.");
-//     }
-// } else {
-//     alert("Usuario bloqueado");
-// }
+  alert("Su usuario ha sido bloqueado. Por favor, comuníquese con el soporte técnico.");
+}
+
+let user = login();
+
+if (user) {
+  let menu = prompt(user.name + 
+    ", elija una opción:\n1. Ver saldo\n2. Ingresar dinero\n3. Transferir dinero\n0. Salir");
+
+  while (menu !== "0") {
+    switch (menu) {
+      case "1":
+        alert("Su saldo es de $" + user.cash);
+        break;
+      case "2":
+        let monto = parseInt(prompt("Ingrese el monto a ingresar:"));
+
+        if (isNaN(monto)) {
+          alert("El monto ingresado no es válido.");
+        } else {
+          user.cash += monto;
+          alert("Se han ingresado $" + monto + ". Su saldo actual es de $" + user.cash);
+        }
+
+        break;
+      case "3":
+        let destino = prompt("Ingrese el usuario al que desea transferir:");
+        let destinatario = buscarUsuario(destino);
+
+        if (!destinatario) {
+          alert("El usuario ingresado no existe.");
+        } else {
+          let monto = parseInt(prompt("Ingrese el monto a transferir:"));
+
+          if (isNaN(monto)) {
+            alert("El monto ingresado no es válido.");
+          } else if (monto > user.cash) {
+            alert("Saldo insuficiente.");
+          } else if (monto > 50000) {
+            alert("El monto ingresado excede el límite diario de transferencia.");
+          } else {
+            user.cash -= monto;
+            destinatario.saldo += monto;
+            alert("Se han transferido $" + monto + " a " + destinatario.name +
+             ". Su saldo actual es de $" + user.cash);
+          }
+        }
+
+        break;
+      default:
+        alert("Opción inválida.");
+    }
+
+    menu = prompt(user.cash +
+         ", elija una opción:\n1. Ver saldo\n2. Ingresar dinero\n3. Transferir dinero\n0. Salir");
+  }
+}
